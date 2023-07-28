965bb1eb93f1ae23ca4ae466c2af455333bc8a6e
Vaishnavi <vaishnavi@Shri-MBP.lan>
2023-07-27 21:38:07 -0700

    Removed duplicate files

--- searchRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const searchController = require('./searchController');
-
-// Search endpoint
-router.get('/', searchController.search);
-
-module.exports = router;
