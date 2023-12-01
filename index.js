77e15268247e6a6e6bb8bdb5e474690e3b049870
Akaash Trivedi <akaash.t1@gmail.com>
2023-12-01 02:16:45 -0800

    AWS EC2 Deployment

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require('express');
 const searchRoutes = require('./routes/searchRoutes');
 const permissionsRoute = require('./routes/permissionsRoute');
 const { downloadCSV } = require('./controllers/searchController');
-
+const path = require('path')
 
 const app = express();
 const port = 5001;
 
 app.use('/search', searchRoutes);
+
+// change for deployment
+const _dirname=path.dirname("")
+const buildpath = path.join(_dirname,"../sar-frontend/build")
+app.use(express.static(buildpath));
 
 // Endpoint for CSV download
 app.get('/download-csv', downloadCSV);
