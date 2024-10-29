10e465f39de4bd6c4891d0813a06d00873cf8563
Varun Mangla <Varunm57@outlook.com>
2024-10-28 18:59:19 -0700

    added scraping by appID back

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -51,26 +51,45 @@ const searchController = async (req, res) => {
     console.error("Missing search query");
     return res.status(400).json({ error: "Search query is missing.\n" });
   }
-
   try {
-    // Search for the main query
     const mainResults = await search({ term: query, country: country });
-    // Secondary search for each primary result
     const relatedResults = [];
-    for (const mainResult of mainResults) {
-      console.log("[%s] Main Title Fetched: %s %s\n", file_name, mainResult.title, country);
-      const relatedQuery = `related to ${mainResult.title}`;
-      relatedResults.push(await search({ term: relatedQuery, country: country }));
+    // if an appID is passed as the query
+    try {
+      const fetched_appID = await app({ appId: query, country: country }); // checking if our query is a valid app ID
+      console.log("App ID passed\n");
+      mainResults.splice(0, mainResults.length, fetched_appID);
+    } catch (error) {
+      // Secondary search for each primary result if required
+      console.log("App ID not passed\n");
+      for (const mainResult of mainResults) {
+        console.log(
+          "[%s] Main Title Fetched: %s\n",
+          file_name,
+          mainResult.title
+        );
+        const relatedQuery = `related to ${mainResult.title}`;
+        relatedResults.push(
+          await search({ term: relatedQuery, country: country })
+        );
 
-      // Introduce a delay between requests (e.g., 1 second)
-      await new Promise((resolve) => setTimeout(resolve, 3000));
+        // Introduce a delay between requests (e.g., 1 second)
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+      }
     }
-
     // Combine the main and secondary results
     const allResults = [
-      ...mainResults.map((result) => ({ ...result, country: country, source: "primary search" })),
+      ...mainResults.map((result) => ({
+        ...result,
+        country: country,
+        source: "primary search",
+      })),
       ...relatedResults.flatMap((results) =>
-        results.map((result) => ({ ...result, country: country, source: "related app" }))
+        results.map((result) => ({
+          ...result,
+          country: country,
+          source: "related app",
+        }))
       ),
     ];
 
@@ -88,7 +107,10 @@ const searchController = async (req, res) => {
       allResults.map(async (appInfo) => {
         try {
           await new Promise((resolve) => setTimeout(resolve, 2000));
-          const appDetails = await app({ appId: appInfo.appId, country: country });
+          const appDetails = await app({
+            appId: appInfo.appId,
+            country: country,
+          });
           return { ...appInfo, ...appDetails };
         } catch (error) {
           console.error("Error fetching app details:", error);
@@ -212,7 +234,7 @@ const searchController = async (req, res) => {
     for (const result of csvData) {
       console.log("[%s] %s\n", file_name, result.title);
     }
-    const pushQuery = "c:"+country+"_t:"+query+"_p:"+permissions; // new relog key since results are based on country + permissions + search query now
+    const pushQuery = "c:" + country + "_t:" + query + "_p:" + permissions; // new relog key since results are based on country + permissions + search query now
     // we want users to get the CSV results corresponding to their entire search, so an update was necessary
     node_ttl.push(pushQuery, csvData, null, 604800); // 1 week
     console.log("CSV stored on backend");
@@ -247,11 +269,10 @@ const downloadRelog = (req, res) => {
       // Implement store and country when applicable
       // Also, add additional options when applicable
       const logInfo_arr = Object.entries(logInfo);
-      for (var i = 0; i < logInfo_arr.length; i++)
-      {
-        logInfo_arr[i] = logInfo_arr[i].join(': ');
+      for (var i = 0; i < logInfo_arr.length; i++) {
+        logInfo_arr[i] = logInfo_arr[i].join(": ");
       }
-      const fileText = logInfo_arr.join('\n');
+      const fileText = logInfo_arr.join("\n");
       console.log(fileText);
       const filename = "Reproducibility_Log_" + logInfo["query"];
       res.setHeader(
@@ -277,25 +298,34 @@ const downloadCSV = (req, res) => {
     try {
       // Use the existing jsonToCsv method to convert JSON to CSV
       const query = req.query.query;
-      const permissions = req.query.includePermissions === 'true';
+      const permissions = req.query.includePermissions === "true";
       const country = req.query.countryCode;
-      const pushQuery = "c:"+country+"_t:"+query+"_p:"+permissions; // to retrieve the csv information from node_ttl
+      const pushQuery = "c:" + country + "_t:" + query + "_p:" + permissions; // to retrieve the csv information from node_ttl
       console.log("pushQuery used for CSV: %s\n", pushQuery);
       var csvInfo = node_ttl.get(pushQuery); // now that we're separating searches by country, we want to make sure users get the CSV response from the country they searched for
-      const csv = jsonToCsv(csvInfo, 'app', standardPermissionsList, permissions);
+      const csv = jsonToCsv(
+        csvInfo,
+        "app",
+        standardPermissionsList,
+        permissions
+      );
       // Get the current timestamp in the desired format
-      const timestamp = new Date().toLocaleString('en-US', {
-        month: 'numeric',
-        day: 'numeric',
-        year: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
+      const timestamp = new Date().toLocaleString("en-US", {
+        month: "numeric",
+        day: "numeric",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
         hour12: true,
       });
 
       // Extract date and time components and remove spaces
-      const dateComponents = timestamp.split('/').map(component => component.trim());
-      const timeComponents = dateComponents[2].split(',').map(component => component.trim());
+      const dateComponents = timestamp
+        .split("/")
+        .map((component) => component.trim());
+      const timeComponents = dateComponents[2]
+        .split(",")
+        .map((component) => component.trim());
 
       // Format the timestamp without spaces
       const formattedTimestamp = `${dateComponents[0]}${dateComponents[1]}${timeComponents[0]}`;
@@ -303,8 +333,8 @@ const downloadCSV = (req, res) => {
       // Suggest a filename to the browser
       const suggestedFilename = `${query}_${formattedTimestamp}.csv`;
 
-      res.setHeader('Access-Control-Expose-Headers', 'Content-Disposition');
-      
+      res.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
+
       // Set response headers for CSV download
       res.setHeader(
         "Content-Disposition",
