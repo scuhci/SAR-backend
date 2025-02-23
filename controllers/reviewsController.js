682b7cbcaf8e0f9fa1333d007646223c19d71b3e
Varun Mangla <Varunm57@outlook.com>
2025-02-22 17:27:25 -0800

    added review relogs

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -1,21 +1,64 @@
 const gplay = require("google-play-scraper");
-const {jsonToCsv} = require('../utilities/jsonToCsv');
+const { jsonToCsv } = require("../utilities/jsonToCsv");
+const cors = require("cors");
+const json_raw = require("../package.json");
+const nodeTTL = require("node-ttl");
+var node_ttl = new nodeTTL();
+
 
 const MAX_REVIEWS_COUNT = 100000; //Fixed value for now, can be updated for future development
+
+const downloadReviewsRelog = (req, res) => {
+  cors()(req, res, () => {
+    try {
+      const logInfo = {
+        version: json_raw.version,
+        date_time: new Date(),
+        store: req.query.store,
+        country: req.query.countryCode,
+        num_reviews: node_ttl.get("c:" + req.query.countryCode + "_a:" + req.query.appId),
+        appId: req.query.appId,
+        sort: req.query.sorting, // must update when we add different sorting
+        info: "This search was performed using the SMAR tool: www.smar-tool.org. This reproducibility log can be used in the supplemental materials of a publication to allow other researchers to reproduce the searches made to gather these results.",
+      };
+      // Implement store and country when applicable
+      // Also, add additional options when applicable
+      const logInfo_arr = Object.entries(logInfo);
+      for (var i = 0; i < logInfo_arr.length; i++) {
+        logInfo_arr[i] = logInfo_arr[i].join(": ");
+      }
+      const fileText = logInfo_arr.join("\n");
+      console.log(fileText);
+      const filename = "Reproducibility_Log_" + logInfo["query"];
+      res.setHeader(
+        "Content-Disposition",
+        `attachment; filename="${filename}"`
+      );
+      res.setHeader("Content-Type", "text/plain");
+      res.send(fileText);
+    } catch (error) {
+      console.error("Error generating relog file:", error);
+      res.status(500).send("An error occurred while generating the relog.");
+    }
+  });
+};
 
 const fetchReviews = async (appId, reviewsCount, countryCode) => {
   const options = {
     appId: appId,
     sort: gplay.sort.NEWEST,
-    lang: 'en',
-    country: countryCode
+    lang: "en",
+    country: countryCode,
   };
 
   try {
     let reviews = [];
     let nextToken;
     let totalFetched = 0;
-    let numReviews = reviewsCount && reviewsCount < MAX_REVIEWS_COUNT ? reviewsCount : MAX_REVIEWS_COUNT;
+    let numReviews =
+      reviewsCount && reviewsCount < MAX_REVIEWS_COUNT
+        ? reviewsCount
+        : MAX_REVIEWS_COUNT;
     console.log(`Fetching ${numReviews} Reviews for AppId: ${appId}`);
 
     while (totalFetched < numReviews) {
@@ -27,12 +70,17 @@ const fetchReviews = async (appId, reviewsCount, countryCode) => {
       const newData = result.data || [];
 
       // Process each review to include the criteria section
-      const processedReviews = newData.map(review => {
+      const processedReviews = newData.map((review) => {
         const criterias = review.criterias || []; // Get criterias section
-        const criteriaData = criterias.map(criteria => `criteria: ${criteria.criteria}: rating: ${criteria.rating}`).join('; '); // Convert criterias to string
+        const criteriaData = criterias
+          .map(
+            (criteria) =>
+              `criteria: ${criteria.criteria}: rating: ${criteria.rating}`
+          )
+          .join("; "); // Convert criterias to string
         return {
           ...review,
-          criterias: criteriaData 
+          criterias: criteriaData,
         };
       });
 
@@ -42,7 +90,7 @@ const fetchReviews = async (appId, reviewsCount, countryCode) => {
       nextToken = result.nextPaginationToken;
     }
 
-    if(reviews.length > numReviews){
+    if (reviews.length > numReviews) {
       reviews = reviews.slice(0, numReviews);
     }
 
@@ -65,7 +113,8 @@ const scrapeReviews = async (req, res) => {
     // Get the actual count of reviews
     const appDetails = await gplay.app({ appId: appId, country: countryCode });
     const reviewsCount = appDetails.reviews;
-
+    const pushQuery = "c:" + countryCode + "_a:" + appId; 
+    node_ttl.push(pushQuery, reviewsCount, null, 604800); // 1 week
     console.log(`App ${appId} contains ${reviewsCount} reviews`);
 
     // Fetch reviews based on the count or the maximum limit
@@ -79,20 +128,23 @@ const scrapeReviews = async (req, res) => {
 
     res.set("Access-Control-Allow-Origin", "*");
     const country_reviews = [
-      ...reviews.map((review) => ({ ...review, country: countryCode}))];
+      ...reviews.map((review) => ({ ...review, country: countryCode })),
+    ];
 
     // Convert reviews data to CSV format
-    const csvData = jsonToCsv(country_reviews, 'reviews');
+    const csvData = jsonToCsv(country_reviews, "reviews");
 
-    res.setHeader('Access-Control-Expose-Headers', 'Content-Disposition');
+    res.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
 
     // Set response headers for CSV download
-    res.setHeader('Content-Disposition', `attachment; filename="${appId}_reviews.csv"`);
-    res.setHeader('Content-Type', 'text/csv');
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename="${appId}_reviews.csv"`
+    );
+    res.setHeader("Content-Type", "text/csv");
 
     // Send the CSV data as a response
     res.status(200).send(csvData);
-
   } catch (error) {
     console.error("Error getting reviews:", error);
     res.status(500).json({ error: "An error occurred while getting reviews." });
@@ -101,4 +153,5 @@ const scrapeReviews = async (req, res) => {
 
 module.exports = {
   scrapeReviews,
+  downloadReviewsRelog,
 };
