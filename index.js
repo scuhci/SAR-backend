2dd4fd2e64de9e7925321568fd6aa62bb8d1272c
Varun Mangla <Varunm57@outlook.com>
2025-03-02 16:14:52 -0800

    reviews relog for ios

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const searchRoutes = require('./routes/searchRoutes');
 const { downloadRelog, downloadCSV } = require('./controllers/searchController');
-const { scrapeReviews } = require('./controllers/reviewsController'); 
+const { scrapeReviews, downloadReviewsRelog } = require('./controllers/reviewsController'); 
 const { scrapeList, downloadTopChartsCSV, downloadTopChartsRelog} = require('./controllers/listController');
 const path = require('path');
 const app = express();
-const port = 5002;
+const port = 5001;
 
 // change for deployment
 const _dirname = path.dirname("");
@@ -13,13 +13,14 @@ const buildpath = path.join(_dirname, "../sar-frontend/build");
 app.use(express.static(buildpath));
 
 // API Endpoints
-app.use('/search', searchRoutes);
-app.get('/download-relog', downloadRelog);
-app.get('/download-csv', downloadCSV);
-app.use('/reviews', scrapeReviews);
-app.use('/toplists', scrapeList)
-app.use('/download-top-relog', downloadTopChartsRelog);
-app.use('/download-top-csv', downloadTopChartsCSV);
+app.use('/ios/search', searchRoutes);
+app.get('/ios/download-relog', downloadRelog);
+app.get('/ios/download-csv', downloadCSV);
+app.use('/ios/reviews', scrapeReviews);
+app.use('/ios/download-reviews-relog', downloadReviewsRelog);
+app.use('/ios/toplists', scrapeList)
+app.use('/ios/download-top-relog', downloadTopChartsRelog);
+app.use('/ios/download-top-csv', downloadTopChartsCSV);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
