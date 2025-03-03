c5ebe143b24e957f4ec066fd509853498282e693
Varun Mangla <Varunm57@outlook.com>
2025-03-02 16:27:08 -0800

    fixing more routing issues.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { scrapeReviews, downloadReviewsRelog } = require('./controllers/reviewsCo
 const { scrapeList, downloadTopChartsCSV, downloadTopChartsRelog} = require('./controllers/listController');
 const path = require('path');
 const app = express();
-const port = 5001;
+const port = 5002;
 
 // change for deployment
 const _dirname = path.dirname("");
@@ -13,14 +13,14 @@ const buildpath = path.join(_dirname, "../sar-frontend/build");
 app.use(express.static(buildpath));
 
 // API Endpoints
-app.use('search', searchRoutes);
-app.get('download-relog', downloadRelog);
-app.get('download-csv', downloadCSV);
-app.use('reviews', scrapeReviews);
-app.use('download-reviews-relog', downloadReviewsRelog);
-app.use('toplists', scrapeList)
-app.use('download-top-relog', downloadTopChartsRelog);
-app.use('download-top-csv', downloadTopChartsCSV);
+app.use('/search', searchRoutes);
+app.get('/download-relog', downloadRelog);
+app.get('/download-csv', downloadCSV);
+app.use('/reviews', scrapeReviews);
+app.use('/download-reviews-relog', downloadReviewsRelog);
+app.use('/toplists', scrapeList)
+app.use('/download-top-relog', downloadTopChartsRelog);
+app.use('/download-top-csv', downloadTopChartsCSV);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
